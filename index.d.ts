c9afad164e86f1f761d452aa4ba4bd52b204a778
Russel Werner <rus.werner@gmail.com>
2017-04-11 10:27:59 +1200

    add babelrc, ts def

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+// General UI Colours
+export const WHITE = '#FFFFFF';
+export const BLACK = '#000000';
+export const BLACKISH = '#2D3034';
+
+export const GREY_900 = '#343C46';
+export const GREY_800 = '#363F49';
+export const GREY_700 = '#434D59';
+export const GREY_600 = '#646E7F';
+export const GREY_500 = '#798293';
+export const GREY_400 = '#939DAF';
+export const GREY_300 = '#BDC4D0';
+
+export const BLUE = '#2286FB';
+export const BLUE_DARK = '#0E74E6';
+export const IVORY = '#F0F2F3';
+export const IVORY_WHITE = '#F4F4F4';
+
+export const MINT = '#2ED8B1';
+export const SPEARMINT = '#29CDA8';
+export const CORAL = '#FF6969';
+export const RED = '#F15354';
+
+// Specific UI Element Colours
+export const SNAPPING_PINK = '#FF00FF';
+export const COMPONENT_PURPLE = '#9C66FD';
+export const TIMELINE_YELLOW = '#FFE118';
+
+export function inactive(colour: string): string;
+export function hover(colour: string): string;
+export function active(colour: string): string;
+export function selected(colour: string): string;
+export function pressed(colour: string): string;
+export function disabled(colour: string): string;
